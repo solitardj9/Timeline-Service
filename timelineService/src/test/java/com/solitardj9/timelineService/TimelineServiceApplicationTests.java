ac2ed4c2cae56b3517b653da637703d1423dbbe8
HoYoung.Rhee <HoYoung.Rhee@DESKTOP-AR0O3VE>
2020-09-05 18:29:37 +0900

    1st Commit

---
+++ timelineService/src/test/java/com/solitardj9/timelineService/TimelineServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solitardj9.timelineService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimelineServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
