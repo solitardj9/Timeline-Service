dfe3c34e48e97d16f2c7065827deda1f99727f38
ssdadmin <ssdadmin@T-Server2>
2020-09-11 11:49:52 +0900

    Timeline Service Commit
    - modify JsonUtil

--- timelineService/src/main/java/com/solitardj9/timelineService/utils/jsonUtil/JsonKeyPathObject.java
+++ timelineService/src/main/java/com/solitardj9/timelineService/utils/jsonUtil/JsonKeyPathObject.java
@@ -56,6 +56,8 @@ public class JsonKeyPathObject {
 	}
 
 	public String getKeyPath() {
+		if (getPath() == null || getPath().isEmpty())
+			return getKey();
 		return getPath() + "." + getKey();
 	}
 	
